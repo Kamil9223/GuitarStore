935fe768c56ab98751822df07e8b0f4e6a107f8e
Kamil Mikus <kamilmikus23@gmail.com>
2022-01-29 10:07:19 +0100

    add GuitarStore

---
+++ GuitarStore/Warehouse.Domain/Store/GuitarStore.cs
@@ -0,0 +1,27 @@
+﻿using Domain;
+
+namespace Warehouse.Domain.Store;
+
+public class GuitarStore : Entity, IIdentifiable
+{
+    public int Id { get; }
+    public string Name { get; }
+    public StoreLocation Location { get; }
+    public ICollection<AcousticGuitar.AcousticGuitar> AcousticGuitars { get; }
+    public ICollection<ElectricGuitar.ElectricGuitar> ElectricGuitars { get; }
+
+    private GuitarStore(string name, StoreLocation location)
+    {
+        Name = name;
+        Location = location;
+        AcousticGuitars = new List<AcousticGuitar.AcousticGuitar>();
+        ElectricGuitars = new List<ElectricGuitar.ElectricGuitar>();
+    }
+
+    public static GuitarStore Create(string name, StoreLocation storeLocation)
+    {
+        return new GuitarStore(name, storeLocation);
+    }
+
+
+}
---
+++ GuitarStore/Warehouse.Domain/Store/StoreLocation.cs
@@ -0,0 +1,24 @@
+﻿using Domain;
+
+namespace Warehouse.Domain.Store;
+
+public class StoreLocation : ValueObject
+{
+    public string Address { get; }
+    public string PostalCode { get; }
+    public string City { get; }
+
+    private StoreLocation(string address, string postalCode, string city)
+    {
+        Address = address;
+        PostalCode = postalCode;
+        City = city;
+    }
+
+    public static StoreLocation Create(string address, string postalCode, string city)
+    {
+        //check rules
+
+        return new StoreLocation(address, postalCode, city);
+    }
+}
