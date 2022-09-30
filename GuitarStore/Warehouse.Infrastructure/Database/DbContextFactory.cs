1529032d469a3df62c370d6987a8cbb6fa02cb6d
Kamil Mikus <kamilmikus23@gmail.com>
2022-09-30 19:10:17 +0200

    Update database

---
+++ GuitarStore/Warehouse.Infrastructure/Database/DbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Warehouse.Infrastructure.Database;
+
+internal class DbContextFactory : IDesignTimeDbContextFactory<WarehouseDbContext>
+{
+    public DbContextFactory() { }
+
+    public WarehouseDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<WarehouseDbContext>();
+        var connectionString = Environment.GetEnvironmentVariable("ASPNETCORE_DB_STRING");
+
+        var sqlConnectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
+
+        optionsBuilder.UseSqlServer(sqlConnectionStringBuilder.ConnectionString);
+        return new WarehouseDbContext(optionsBuilder.Options);
+    }
+}
