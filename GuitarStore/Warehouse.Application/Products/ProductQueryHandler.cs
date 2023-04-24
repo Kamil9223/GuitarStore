bc69b561b12ed821da2e2780f52e7fdc3d384da3
Kamil Mikus <kamilmikus23@gmail.com>
2023-04-24 22:16:56 +0200

    Work In Progress

---
+++ GuitarStore/Warehouse.Application/Products/ProductQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warehouse.Application.Products.Queries;
+
+namespace Warehouse.Application.Products;
+
+internal class ProductQueryHandler :
+    IQueryHandler<ProductDetailsQuery>,
+    IQueryHandler<ListProductsQuery>
+{
+    
+}
--- GuitarStore/Warehouse.Application/Products/Queries/ProductDetailsQuery.cs
+++ GuitarStore/Warehouse.Application/Products/Queries/ProductDetailsQuery.cs
@@ -1,12 +1,8 @@
 ﻿using Application;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Warehouse.Application.Products.Queries;
 
 public class ProductDetailsQuery : IQuery
 {
+    public int ProductId { get; set; }
 }
