ccd3fb2c60310328c8a0191468e7827fffbd8b74
Kamil Mikus <kamilmikus23@gmail.com>
2023-07-22 12:16:49 +0200

    Update Db schema

---
+++ GuitarStore/Catalog.Infrastructure/Migrations/20230716161404_UpdateSchema.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Warehouse.Infrastructure.Migrations
+{
+    public partial class UpdateSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Catalog");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                schema: "Warehouse",
+                newName: "Products",
+                newSchema: "Catalog");
+
+            migrationBuilder.RenameTable(
+                name: "Categories",
+                schema: "Warehouse",
+                newName: "Categories",
+                newSchema: "Catalog");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Warehouse");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                schema: "Catalog",
+                newName: "Products",
+                newSchema: "Warehouse");
+
+            migrationBuilder.RenameTable(
+                name: "Categories",
+                schema: "Catalog",
+                newName: "Categories",
+                newSchema: "Warehouse");
+        }
+    }
+}
